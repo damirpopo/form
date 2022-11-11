665f61c84261651eb413c9e9df8faa2398e36965
damirpopo <43522476+damirpopo@users.noreply.github.com>
2022-11-11 13:19:49 +0300

    Add files via upload

---
+++ lesson js 7/assets/js/main.js
@@ -0,0 +1,43 @@
+// задание 1
+// let option = new Option ('class','class');
+// genres.append(option)
+
+// // задание 2
+// let area = null;
+// let view = document.getElementById('view');
+// view.onclick = function() {
+//   editStart();
+// };
+// function editStart() {
+//   area = document.createElement('textarea');
+//   area.className = 'edit';
+//   area.value = view.innerHTML;
+//   area.onkeydown = function(event) {
+//     if (event.key == 'Enter') {
+//       this.blur();
+//     }
+//   };
+//   area.onblur = function() {
+//     editEnd();
+//   };
+//   view.replaceWith(area);
+//   area.focus();
+// }
+// function editEnd() {
+//   view.innerHTML = area.value;
+//   area.replaceWith(view);
+// }
+
+// задание 4
+// var op;   
+//   function func(){
+//     var num1 = Number(document.getElementById("num1").value);
+//     var num2 = Number(document.getElementById("num2").value);
+//     var num3 = Number(document.getElementById("vklad").value);
+//     var result = num1*(num3+(num3/(1+num3)*num2));
+//     var result_old=num1
+//     document.getElementById("result").innerHTML = result;
+//     document.getElementById("result_old").innerHTML = result_old;
+//     }
+    
+  
